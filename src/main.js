7697424d034810cd520f72749389b827f961a968
5kydriven <tagpuno619@gmail.com>
2024-03-01 09:01:40 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import "bootstrap/dist/css/bootstrap.css";
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
+import './assets/main.css'
 
-createApp(App).use(router).use(bootstrap).mount("#app");
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,9 @@
-<template>
-  <div class="home">
-    <HelloWorld msg="In progress" />
-  </div>
-  <Modal></Modal>
-</template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-import Modal from "@/components/Modal.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-    Modal
-  },
-};
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
 </script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
