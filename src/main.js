3c22c2f26c197dbddf7b36483782f1d209972f67
5kydriven <tagpuno619@gmail.com>
2024-04-05 15:27:33 +0800

    init flowbite

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { initFlowbite } from 'flowbite'
+
 
 import App from './App.vue'
 import router from './router'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/js/bootstrap'
+import '@/assets/tailwind.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(initFlowbite())
 
 app.mount('#app')
