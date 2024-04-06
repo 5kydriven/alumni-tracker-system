b1fd9ea2f4c5ad90534587e6843faeac20453885
5kydriven <tagpuno619@gmail.com>
2024-04-06 09:49:09 +0800

    first commit

---
+++ src/components/SubAdmin/AlumniPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="p-4 md:ml-64">
+    <div class="p-4 border-2 border-gray-200 border-dashed dark:border-gray-700 mt-14">
+      <!-- first coloumn -->
+      <h1 class="mb-3 font-semibold text-xl">Alumni's Information</h1>
+
+      <div class="grid grid-cols-2 md:grid-cols-3 gap-4 mb-6">
+        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+          <p class="md:text-base text-gray-400 dark:text-gray-500 sm:text-sm">total alumni's</p>
+        </div>
+        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+          <p class="md:text-base text-gray-400 dark:text-gray-500 sm:text-sm">Employed</p>
+        </div>
+        <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+          <p class="md:text-base text-gray-400 dark:text-gray-500 sm:text-sm">Unemployed</p>
+        </div>
+      </div>
+      <hr class="border-2" />
+      
+      <!-- Second Col -->
+      <h1 class="my-3 font-semibold text-xl">All Alumni's (12,000)</h1>
+      <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+        Tables
+      </div>
+    </div>
+  </div>
+</template>
