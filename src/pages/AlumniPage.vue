5f386579ef53fa91c8938f62a06ba8f9ce1494e7
5kydriven <tagpuno619@gmail.com>
2024-05-16 22:58:01 +0800

    working datatable

--- src/pages/AlumniPage.vue
+++ src/pages/AlumniPage.vue
@@ -1,223 +1,130 @@
 <template>
     <div class="card">
-        <DataTable v-model:filters="filters" :value="customers" scrollable scrollHeight="450px" showGridlines :rows="10"
-            dataKey="id" filterDisplay="menu" :loading="loading" size="small" resizableColumns columnResizeMode="fit"
-            removableSort :globalFilterFields="['name', 'country.name', 'representative.name', 'balance', 'status',]">
+        <DataTable v-model:filters="filters" :value="store.alumni" ref="dt" scrollable scroll-height="450px" showGridlines
+            removableSort filterDisplay="menu">
             <template #header>
                 <div class="flex justify-between">
                     <div class="flex gap-2">
                         <Button type="button" icon="pi pi-filter-slash" label="Clear" outlined @click="clearFilter()" />
-                        <Button label="New" icon="pi pi-plus" severity="success" class="mr-2" @click="openNew" />
+                        <Button type="button" icon="pi pi-plus" label="Add" outlined @click="openNew" />
+                        <Button type="button" icon="pi pi-external-link" label="Export" outlined @click="exportCSV($event)"/>
+
                     </div>
-                    <IconField iconPosition="left">
-                        <InputIcon>
-                            <i class="pi pi-search" />
-                        </InputIcon>
-                        <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
-                    </IconField>
+                    <span class="relative">
+                        <i class="pi pi-search absolute top-2/4 -mt-2 left-3 text-surface-400 dark:text-surface-600" />
+                        <InputText v-model="filters['global'].value" placeholder="Keyword Search"
+                            class="pl-10 font-normal" />
+                    </span>
                 </div>
             </template>
-            <template #empty> No customers found. </template>
-            <template #loading> Loading customers data. Please wait. </template>
-            <Column field="name" header="Name" sortable style="max-width: 10rem">
-                <template #body="{ data }">
-                    <div class="flex align-items-center gap-2">
-                        <img :alt="data.representative.name"
-                            :src="`https://primefaces.org/cdn/primevue/images/avatar/${data.representative.image}`"
-                            style="width: 32px" />
-                        <span>{{ data.name }}</span>
-                    </div>
-                </template>
-                <template #filter="{ filterModel }">
-                    <InputText v-model="filterModel.value" type="text" class="p-column-filter"
-                        placeholder="Search by name" />
+            <template #empty> No alumni found. </template>
+
+            <Column header="No.">
+                <template #body="slotProps">
+                    {{ slotProps.index + 1 }}
                 </template>
             </Column>
-            <Column header="Batch" filterField="date" dataType="date" style="max-width: 10rem">
-                <template #body="{ data }">
-                    {{ formatDate(data.date) }}
-                </template>
-                <template #filter="{ filterModel }">
-                    <Calendar v-model="filterModel.value" dateFormat="mm/dd/yy" placeholder="mm/dd/yyyy"
-                        mask="99/99/9999" />
-                </template>
+
+            <Column field="fullname" sortable header="Name">
             </Column>
-            <Column header="Contact Number" filterField="balance" dataType="numeric" style="max-width: 10rem">
-                <template #body="{ data }">
-                    {{ formatCurrency(data.balance) }}
-                </template>
+
+            <Column field="contactnumber" header="Contact"></Column>
+
+            <Column field="course" header="Course" :showFilterMatchModes="false">
                 <template #filter="{ filterModel }">
-                    <InputNumber v-model="filterModel.value" mode="currency" currency="USD" locale="en-US" />
-                </template>
-            </Column>
-            <Column header="Status" field="status" :filterMenuStyle="{ width: '14rem' }" style="max-width: 10rem">
-                <template #body="{ data }">
-                    <Tag :value="data.status" :severity="getSeverity(data.status)" />
-                </template>
-                <template #filter="{ filterModel }">
-                    <Dropdown v-model="filterModel.value" :options="statuses" placeholder="Select One"
+                    <Dropdown v-model="filterModel.value" :options="courses" placeholder="Select One"
                         class="p-column-filter" showClear>
                         <template #option="slotProps">
-                            <Tag :value="slotProps.option" :severity="getSeverity(slotProps.option)" />
+                            <Tag :value="slotProps.option" />
                         </template>
                     </Dropdown>
                 </template>
             </Column>
-            <Column field="verified" header="Verified" dataType="boolean" bodyClass="text-center"
-                style="max-width: 5rem">
-                <template #body="{ data }">
-                    <i class="pi"
-                        :class="{ 'pi-check-circle text-green-500 ': data.verified, 'pi-times-circle text-red-500': !data.verified }"></i>
-                </template>
+
+            <Column field="campus" header="Campus"></Column>
+
+            <Column field="batch" header="Batch" :showFilterMatchModes="false">
                 <template #filter="{ filterModel }">
-                    <label for="verified-filter" class="font-bold"> Verified </label>
-                    <TriStateCheckbox v-model="filterModel.value" variant="filled"/>
+                    <Dropdown v-model="filterModel.value" :options="batches" placeholder="Select One"
+                        class="p-column-filter" showClear>
+                        <template #option="slotProps">
+                            <Tag :value="slotProps.option" />
+                        </template>
+                    </Dropdown>
                 </template>
             </Column>
-            <Column field="Actions" header="Actions" bodyClass="text-center" style="max-width: 8rem">
-                <template #body="{ data }">
-                    <button @click.prevent="OpToggle" class="cursor-pointer hover:text-gray-800 text-gray-500 border py-0">
-                        <i class="pi pi-ellipsis-h line-hei"></i>
-                    </button>
-                    <OverlayPanel ref="op">
-                        <div class="text-base list-none bg-white">
-                            <ul class="py-1" role="none">
-                                <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Show Profile</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Edit</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </OverlayPanel>
+
+            <Column field="employment.status" header="Status" :showFilterMatchModes="false">
+                <template #filter="{ filterModel }">
+                    <Dropdown v-model="filterModel.value" :options="employment" placeholder="Select One"
+                        class="p-column-filter" showClear>
+                        <template #option="slotProps">
+                            <Tag :value="slotProps.option" />
+                        </template>
+                    </Dropdown>
                 </template>
             </Column>
+
+            <Column header="Actions"></Column>
         </DataTable>
     </div>
 
-    <Dialog v-model:visible="productDialog" :style="{ width: '450px' }" header="Product Details" :modal="true"
+    <Dialog v-model:visible="productDialog" :style="{ width: '700px' }" header="Add Alumni" :modal="true"
         class="p-fluid">
-        <span class="p-text-secondary block mb-5">Update your information.</span>
-        <div class="flex align-items-center gap-3 mb-3">
-            <label for="username" class="font-semibold w-6rem">Username</label>
-            <InputText id="username" class="flex-auto" autocomplete="off" />
-        </div>
-        <div class="flex align-items-center gap-3 mb-5">
-            <label for="email" class="font-semibold w-6rem">Email</label>
-            <InputText id="email" class="flex-auto" autocomplete="off" />
-        </div>
-        <div class="flex justify-content-end gap-2">
-            <Button type="button" label="Cancel" severity="secondary" @click="openNew"></Button>
-            <Button type="button" label="Save" @click="openNew"></Button>
-        </div>
+        <AlumniForm @cancel="openNew" />
     </Dialog>
     <Toast />
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { CustomerService } from '@/service/CustomerService';
-import { FilterMatchMode, FilterOperator } from 'primevue/api';
+import AlumniForm from '@/components/AlumniForm.vue';
+import { useAlumniStore } from '@/stores/AlumniStore';
+import { onMounted, ref } from 'vue';
 import { useToast } from 'primevue/usetoast';
-import TriStateCheckbox from 'primevue/tristatecheckbox';
+
 const toast = useToast();
 
+const store = useAlumniStore();
 
-const op = ref()
-
-const OpToggle = (event) => {
-    op.value.toggle(event);
-}
-
-const customers = ref();
 const filters = ref();
-const representatives = ref([
-    { name: 'Amy Elsner', image: 'amyelsner.png' },
-    { name: 'Anna Fali', image: 'annafali.png' },
-    { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
-    { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
-    { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
-    { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
-    { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
-    { name: 'Onyama Limba', image: 'onyamalimba.png' },
-    { name: 'Stephen Shaw', image: 'stephenshaw.png' },
-    { name: 'XuXue Feng', image: 'xuxuefeng.png' }
-]);
-const statuses = ref(['unqualified', 'qualified', 'new', 'negotiation', 'renewal', 'proposal']);
-const loading = ref(true);
+const courses = ref(['BSIT', 'BSCRIM'])
+const batches = ref([2026, 2024])
+const employment = ref(['Not Track', 'Unemployed'])
 const productDialog = ref(false)
-
-onMounted(() => {
-    CustomerService.getCustomersMedium().then((data) => {
-        customers.value = getCustomers(data);
-        loading.value = false;
-    });
-});
+const dt = ref();
 
 const openNew = () => {
     productDialog.value = !productDialog.value
-    if(!productDialog.value) {
+    if (!productDialog.value) {
         toast.add({ severity: 'success', summary: 'Successful', detail: 'Click', life: 2000 });
     }
 }
 
 const initFilters = () => {
     filters.value = {
-        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        name: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-        'country.name': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
-        representative: { value: null, matchMode: FilterMatchMode.IN },
-        date: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }] },
-        balance: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
-        status: { operator: FilterOperator.OR, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
-        activity: { value: [0, 100], matchMode: FilterMatchMode.BETWEEN },
-        verified: { value: null, matchMode: FilterMatchMode.EQUALS }
+        global: { value: null },
+        firstname: { value: null},
+        contactnumber: { value: null},
+        course: { value: null},
+        campus: { value: null},
+        batch: { value: null},
+        'employment.status': { value: null},
+
     };
 };
 
 initFilters();
 
-const formatDate = (value) => {
-    return value.toLocaleDateString('en-US', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric'
-    });
-};
-const formatCurrency = (value) => {
-    return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-};
 const clearFilter = () => {
     initFilters();
 };
-const getCustomers = (data) => {
-    return [...(data || [])].map((d) => {
-        d.date = new Date(d.date);
 
-        return d;
-    });
+const exportCSV = () => {
+    dt.value.exportCSV();
 };
-const getSeverity = (status) => {
-    switch (status) {
-        case 'unqualified':
-            return 'danger';
 
-        case 'qualified':
-            return 'success';
-
-        case 'new':
-            return 'info';
-
-        case 'negotiation':
-            return 'warning';
-
-        case 'renewal':
-            return null;
-    }
-};
+onMounted(async () => {
+    await store.filteredAlumni(); // Wait for the data to be fetched
+    // loading.value = false;
+})
 </script>
